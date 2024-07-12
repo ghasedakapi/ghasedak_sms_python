b824b718a4c2da107fb9a40ae59731cb442e508b
fhasheminasab <hasheminasab97@gmail.com>
2024-07-12 16:45:11 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ghasedak_sms',
-    version='0.1.0',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
         'requests',
